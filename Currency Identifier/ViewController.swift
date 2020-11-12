a55c0b7b4762208a4ca23ce52705a295bb4a27ca
vakas-786 <vakas0959@gmail.com>
2020-11-12 17:32:36 -0500

    Initial Commit

---
+++ Currency Identifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Identifier
+//
+//  Created by Vakas Akhtar on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
